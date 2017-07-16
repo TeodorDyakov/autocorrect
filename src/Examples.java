ab9e7573477983e9dbdec5238068525f37826268
TeodorDyakov <tedy1996@gmail.com>
2017-07-16 04:10:53 +0300

    ex

--- src/Examples.java
+++ src/Examples.java
@@ -1,5 +1,6 @@
 
-
+import suggester.Keyboard;
+import suggester.KeyboardDistanceComparator;
 import suggester.Suggester;
 import util.WordLists;
 
@@ -9,15 +10,21 @@ public class Examples {
 
 		Suggester s = new Suggester(WordLists.LOWERCASE_WORDS);
 
-		System.out.println(s.suggestions("definataly", 2));
-		System.out.println(s.suggestions("ametour", 2));
-		System.out.println(s.suggestions("aceptablle", 2));
-		System.out.println(s.suggestions("acidentaly", 2));
-		System.out.println(s.suggestions("imposible", 2));
-		System.out.println(s.suggestions("spdier", 1));
-		System.out.println(s.suggestions("palidnrome", 2));
-		System.out.println(s.suggestions("nightmar", 1));
-		System.out.println(s.suggestions("cta", 1));
-		System.out.println(s.suggestions("throu", 2));
+		long tic = System.currentTimeMillis();
+		System.out.println(s.suggestions("definataly"));
+		System.out.println(s.suggestions("ametour"));
+		System.out.println(s.suggestions("aceptablle"));
+		System.out.println(s.suggestions("acidentaly"));
+		System.out.println(s.suggestions("imposible"));
+		System.out.println(s.suggestions("spdier"));
+		System.out.println(s.suggestions("palidnrome"));
+		System.out.println(s.suggestions("nightmar"));
+		System.out.println(s.suggestions("cta"));
+		System.out.println(s.suggestions("throu"));
+		System.out.println(s.suggestions("bier"));
+		System.out.println(s.suggestions("brer"));
+		System.out.println(Keyboard.keyboardDistance('e', 'e'));
+		System.out.println(System.currentTimeMillis() - tic);
+		System.out.println(KeyboardDistanceComparator.distance("cat", "act"));
 	}
 }
